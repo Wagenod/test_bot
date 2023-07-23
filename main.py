be6d748138dfa0db9d1e680eafda7347853b4637
Wagenod <wagenod93@mail.ru>
2023-07-23 19:19:04 +0300

    added proxy to bot

--- main.py
+++ main.py
@@ -4,18 +4,14 @@ from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
 import os
 
-
+PROXY_URL = "http://proxy.server:3128"
 load_dotenv()
 
-
-# Объект бота
-bot = Bot(token=os.environ.get("TOKEN"))
-# Диспетчер для бота
+bot = Bot(token=os.environ.get("TOKEN"), proxy=PROXY_URL)
 dp = Dispatcher(bot)
-# Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 
-# Хэндлер на команду /test1
+
 @dp.message_handler(commands="test1")
 async def cmd_test1(message: types.Message):
     options = webdriver.ChromeOptions()
@@ -23,7 +19,6 @@ async def cmd_test1(message: types.Message):
     driver = webdriver.Chrome(options=options)
     driver.get("https://hashdork.com/ru/best-heroku-alternatives/")
     await message.reply(driver.title)
-
 
 if __name__ == "__main__":
     # Запуск бота
